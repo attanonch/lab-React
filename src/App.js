3e0a5fec609b0052f28096ce8bef4693fea72351
attanonch <attanonchupool@gmail.com>
2022-08-17 12:31:06 +0700

    commit1 change word

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import React from 'react';
 import './App.css';
 import WordCard from './WordCard';
 
-const word = "Hello";
+const word = "Attanon";
+const word2 = "Computer";
+const changeword = () => {
+  const word2 = word;
+}
+
 function App() {
   return (
     <div>
       <WordCard value={word}/>
+      <h4>คำใบ้ : ชื่อของผู้จัดทำ</h4>
     </div>
   );
 }
+
 
 export default App;
