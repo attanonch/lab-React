ae5cb7c715a59cce17000161f660264d6c94a579
attanonch <attanonchupool@gmail.com>
2022-08-17 16:23:03 +0700

    commit6 fixprocesss

--- src/App.js
+++ src/App.js
@@ -2,22 +2,22 @@ import React from 'react';
 import './App.css';
 import WordCard from './WordCard';
 
-const word = "Attanon";
-const word2 = "Computer";
-const changeword = () => {
-  <WordCard value={word2}/>
+
+var wordArray = ['Attanon', 'Computer','Java','Apple'];
+var word = wordArray[Math.floor(Math.random()*wordArray.length)];
+
+function refreshPage() {
+  window.location.reload(false);
 }
-
-
 function App() {
   return (
     <div>
       <WordCard value={word}/>
-      <h4>คำใบ้ : ชื่อของผู้จัดทำ</h4>
+      <h4>คำใบ้ : คำศัพท์เกี่ยวกับคอมพิวเตอร์และชื่อคนทำ</h4>
       <button
           type="button"
           className='button'
-          onClick={changeword}
+          onClick={refreshPage}
         >Change word</button>
     </div>
   );
